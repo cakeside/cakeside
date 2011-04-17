bfd787ddf0a8e20e2e3f3f18c4538aa9e32971d1
mo kareem <email@mokhan.ca>
2011-04-17 00:39:00 -0600

    initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cake::Application.config.secret_token = 'ae8cd7c6c34a50f132dc7769418e0ef7f1b9660c8034bd98e334792ceb1774a0a945a5434a38854c904c0e2524aa193c6cc3bf836e330d7d0c4d457dd8fecc29'
