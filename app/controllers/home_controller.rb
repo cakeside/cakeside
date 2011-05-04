bc6d839cc495bc0cd7f3cca0b31ec0e48dde18be
morilla <mo@mokhan.ca>
2011-05-03 18:56:01 -0600

    display the 5 most recent creations.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @creations = Creation.all.take(5).reverse
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @creations }
+    end
   end
 end
