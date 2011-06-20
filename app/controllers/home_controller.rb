f29c746e33e2450c423158fbda207153a6bffa42
mo k <mo@mokhan.ca>
2011-06-19 22:34:53 -0600

    tidy up the edit creation page and remove the rotating banner from all pages except for the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @creations = Creation.all.reverse.take(6)
+    @creations = Creation.all.reverse.take(4)
 
     respond_to do |format|
       format.html # index.html.erb
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
   def index
     @creations = Creation.search params[:q], :include => :user, :match_mode => :boolean
+    @search = params[:q]
   end
 
 end
