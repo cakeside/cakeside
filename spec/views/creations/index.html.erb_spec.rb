94ea583e5120409813237f497b53058e7b86c2c5
mo k <mo@mokhan.ca>
2011-06-20 20:52:46 -0600

    fix broken tests in views/creations/index.html.erb

--- spec/views/creations/index.html.erb_spec.rb
+++ spec/views/creations/index.html.erb_spec.rb
@@ -17,8 +17,8 @@ describe "creations/index.html.erb" do
   it "renders a list of creations" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "div>h2", :text => "Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select "div>p", :text => "MyText... more".to_s, :count => 2
   end
 end
