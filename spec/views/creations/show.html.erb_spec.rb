2338119ba26f2afa6385c24695f759cc87a63de3
mo k <mo@mokhan.ca>
2011-06-21 21:58:48 -0600

    remove dead code from show.html.erb spec.

--- spec/views/creations/show.html.erb_spec.rb
+++ spec/views/creations/show.html.erb_spec.rb
@@ -3,16 +3,7 @@ require 'spec_helper'
 describe "creations/show.html.erb" do
   include Devise::TestHelpers
 
-  def mock_user(stubs={})
-    @mock_user ||= mock_model(User, stubs).as_null_object
-  end
-
-  def mock_creation(stubs={})
-    @mock_creation ||= mock_model(Creation, stubs).as_null_object
-  end
-
   before(:each) do
-    request.env['warden'] = mock(Warden, :authenticate => mock_user, :authenticate! => mock_user)
     @creation = assign(:creation, stub_model(Creation,
       :name => "Name",
       :story => "MyText"
