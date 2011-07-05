2afaa9f9763b53d8bb8ef302e0635c2fa59431f6
mo k <mo@mokhan.ca>
2011-07-04 21:01:08 -0600

    create deploy.rb for basic capistrano deployment options.

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+set :application, "www.cakeside.com"
+set :repository,  "git@cakeartist.unfuddle.com:cakeartist/cake.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/home/ubuntu/apps/#{application}"
+
+role :web, "ec2-50-17-28-146.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-50-17-28-146.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
+role :db,  "ec2-50-17-28-146.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
+
+set :user, "ubuntu"
+set :use_sudo, false
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "amazon-cakeside", "cakeside.pem")] 
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
