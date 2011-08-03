a4cc1551e34015002888051736ee9e7a6e866282
mo k <mo@mokhan.ca>
2011-08-02 21:09:34 -0600

    add checkboxes for each category when uploading a new creation, and update the creation update page.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  has_and_belongs_to_many :creations
+  has_and_belongs_to_many :creations, :join_table => 'creations_categories'
 end
--- app/models/creation.rb
+++ app/models/creation.rb
@@ -3,7 +3,7 @@ class Creation < ActiveRecord::Base
   validates :image,  :presence => true
   attr_accessible :user_id, :story, :name, :image, :remote_image_url
   belongs_to :user
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, :join_table => 'creations_categories', :uniq => true, :autosave => true
   mount_uploader :image, ImageUploader
 
   define_index do
