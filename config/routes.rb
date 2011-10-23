1da9814349170fd751cd994600fb83ebbff5ee08
mo k <m@mokhan.ca>
2011-10-22 19:56:39 -0600

    organize routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,33 @@
 Cake::Application.routes.draw do
-  resources :creations
+  root :to => "home#index"
 
+  # /home
   get "home/index"
+
+  # /creations
+  resources :creations
+  match 'my_creations' => 'creations#mine', :as => 'my_creations', :method => 'GET'
+
+  # /profiles
   get "profiles/index"
   get "profiles/show"
+  match 'profiles/show/:id' => 'profiles#show', :as => 'profile', :method => 'GET'
+
+  # /search
   get "search/index"
 
-  devise_for :users, :controllers => {:registrations => 'registrations'}
-  match '/auth/:provider/callback' => 'authentications#create'
-  match 'my_creations' => 'creations#mine', :as => 'my_creations', :method => 'GET'
+  # /artists
   match 'artists' => 'profiles#index', :as => 'all_profiles', :method => 'GET'
   match 'artists/:id' => 'profiles#show', :as => 'profile', :method => 'GET'
-  match 'profiles/show/:id' => 'profiles#show', :as => 'profile', :method => 'GET'
-  match 'categories/:id' => 'categories#show', :method => 'GET'
-  match 'authentications' => 'authentications#index', :method => 'GET'
 
-  root :to => "home#index"
+  # /categories
+  match 'categories/:id' => 'categories#show', :method => 'GET'
+
+  # /authentications
+  match 'authentications' => 'authentications#index', :method => 'GET'
+  match '/auth/:provider/callback' => 'authentications#create'
+
+  # /users
+  devise_for :users, :controllers => {:registrations => 'registrations'}
+
 end
