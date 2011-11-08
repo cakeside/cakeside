eb099e654a6aacaab1170d4ac298ad46e67e93b2
mo k <m@mokhan.ca>
2011-11-07 21:06:56 -0700

    remove unused action from search controller.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,4 @@ class SearchController < ApplicationController
     @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
     @search = params[:q]
   end
-
-  def tags
-    @creations = Category.where(:slug => params[:id]).first.posts
-    render 'home/index'
-  end
 end
