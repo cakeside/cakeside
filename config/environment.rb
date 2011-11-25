9b615a9f07c3edfd99b1334831df88d78c184f15
mo k <m@mokhan.ca>
2011-11-25 01:11:35 -0700

    add facebook like link, and follow mocheen on twitter link, just for fun.

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Cake::Application.initialize!
 Date::DATE_FORMATS[:foomat] = "%d/%m/%Y"
-Time::DATE_FORMATS[:foomat] = "%B %d %Y"
+Time::DATE_FORMATS[:foomat] = "%B. %d, %Y"
