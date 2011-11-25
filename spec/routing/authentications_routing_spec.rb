4a9c909dcdcbfc8a2aa0406c8554d4fac52c53f1
mo k <m@mokhan.ca>
2011-11-25 01:30:18 -0700

    remove authentications controller routing spec because we no longer have an authentications controller.

--- spec/routing/authentications_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require "spec_helper"
-
-describe AuthenticationsController do
-  describe "routing" do
-    it "routes to #show" do
-      get("/authentications/").should route_to("authentications#index", :method => 'GET')
-    end
-  end
-end
