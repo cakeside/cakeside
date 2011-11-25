8c2a596b18f3445754f818903b2d3ab3ab41b3ee
mo k <m@mokhan.ca>
2011-11-24 22:35:28 -0700

    tidy up the creations/show page.

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -8,6 +8,7 @@ class CreationsController < ApplicationController
   # GET /creations/1
   def show
     @creation = Creation.find(params[:id])
+    @creations = @creation.user.creations.page(params[:page]).per(8)
   end
 
   # GET /creations/new
