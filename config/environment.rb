06c06356ba3ea6ae521e9b5bc9977f03cfa6832b
mo k <m@mokhan.ca>
2011-11-24 23:52:37 -0700

    add last logged in date, and member since date to profile page.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Cake::Application.initialize!
+Date::DATE_FORMATS[:foomat] = "%d/%m/%Y"
+Time::DATE_FORMATS[:foomat] = "%B %d %Y"
