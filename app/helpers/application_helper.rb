93f31928873bf3b0c68e4543eba4cf94849d09ee
luu stiles <luuduong@gmail.com>
2011-12-09 03:42:04 -0700

    fav icon, chef hat, css updates, index showing images again not description.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar_url(user)
-      default_url = "#{root_url}assets/rails.png"
+      default_url = "#{root_url}assets/chefhat_profile.PNG"
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?&d=#{CGI.escape(default_url)}"
   end
