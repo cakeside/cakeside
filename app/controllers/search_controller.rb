072fb968cea6ee4dc65fe254f359472b4b64420f
mo k <m@mokhan.ca>
2011-12-15 21:13:22 -0700

    switch from sphinx search to straight sql search.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def index
-    @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
+    sql = "%"+params[:q]+"%"
+    @creations = Creation.where("name like ? OR story like ?", sql, sql).page(params[:page]).per(6)
+    # @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
     @search = params[:q]
   end
 end
