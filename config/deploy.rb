e8f6e2d562e68d40d3bcf9b431e0f94dae570615
mo k <m@mokhan.ca>
2011-12-18 21:53:35 -0700

    update capistrano deployment to point to the new ec2 server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :deploy_to, "/home/cakeside/apps/#{application}"
 
-role :web, "ec2-50-16-149-107.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
-role :app, "ec2-50-16-149-107.compute-1.amazonaws.com"                          # This may be the same as your `Web` server
-role :db,  "ec2-50-16-149-107.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
+role :web, "ec2-107-22-93-60.compute-1.amazonaws.com"                          # Your HTTP server, Apache/etc
+role :app, "ec2-107-22-93-60.compute-1.amazonaws.com"                         # This may be the same as your `Web` server
+role :db,  "ec2-107-22-93-60.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
 
 set :user, "cakeside"
 set :use_sudo, false
