5e4b9aaaa3b49283143fc978f56a1ea3ceab4b22
mo k <m@mokhan.ca>
2012-01-06 20:38:30 -0700

    remove omniauth related stuff from User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,8 @@
 class User < ActiveRecord::Base
   validates :name,  :presence => true
   has_many :authentications
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-
   has_many :creations, :dependent => :destroy
   has_many :likes, :dependent => :destroy
-
-  def apply_omniauth(omniauth)
-    puts omniauth
-    self.email = omniauth['user_info']['email'] if email.blank?
-    authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
-  end
-  
-  def password_required?
-    (authentications.empty? || !password.blank?) && super
-  end
 end
