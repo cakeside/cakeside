915b6b5842a1c0074661ce04ae474ee694efa32c
mo k <m@mokhan.ca>
2012-01-07 10:44:50 -0700

    add members to the search and display top 8 creations + top 8 members that match.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,8 @@ class SearchController < ApplicationController
       @search = nil
     else
       sql = "%"+params[:q]+"%"
-      @creations = Creation.where("name like ? OR story like ?", sql, sql).page(params[:page]).per(6)
+      @creations = Creation.where("name like ? OR story like ?", sql, sql).page(params[:page]).per(8)
+      @members = User.where("name like ?", sql).page(params[:page]).per(8)
       # @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
       @search = params[:q]
     end
