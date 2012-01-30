24143ba17e6651f020cf6f6f6966f36cc4bef14d
mo k <m@mokhan.ca>
2012-01-29 20:46:31 -0700

    redirect to the home page if no search term is entered.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,16 @@
 class SearchController < ApplicationController
   def index
-    if params[:q].blank? 
-      @creations = []
-      @search = nil
-    else
-      sql = "%"+params[:q]+"%"
-      @creations = Creation.where("upper(name) like upper(?) OR upper(story) like upper(?)", sql, sql).page(params[:page]).per(8)
-      @members = User.where("upper(name) like upper(?)", sql).page(params[:page]).per(8)
-      # @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
-      @search = params[:q]
+    respond_to do |format|
+      if params[:q].blank? 
+        format.html { redirect_to(home_index_url) }
+      else
+        sql = "%"+params[:q]+"%"
+        @creations = Creation.where("upper(name) like upper(?) OR upper(story) like upper(?)", sql, sql).page(params[:page]).per(8)
+        ## @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
+        @members = User.where("upper(name) like upper(?)", sql).page(params[:page]).per(8)
+        @search = params[:q]
+        format.html
+      end
     end
   end
 end
