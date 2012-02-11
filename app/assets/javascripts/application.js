d57d103bd50f768587ebb786b3bdc377b9618be0
mo k <m@mokhan.ca>
2012-02-10 20:53:46 -0700

    start conversion to new version of bootstrap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,12 +9,5 @@
 //= require jquery-ui
 //= require jquery.Jcrop
 //= require jquery.color
-//= require bootstrap-alerts
-//= require bootstrap-buttons
-//= require bootstrap-dropdown
-//= require bootstrap-modal
-//= require bootstrap-popover
-//= require bootstrap-scrollspy
-//= require bootstrap-tabs
-//= require bootstrap-twipsy
+//= require bootstrap
 //= require_tree .
