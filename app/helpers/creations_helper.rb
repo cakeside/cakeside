d07feb75ec40f68a8b011d3a61195798c2d78cae
mo k <mo@mokhan.ca>
2012-02-29 20:42:40 -0700

    display first 25 characters of title in listing page.

--- app/helpers/creations_helper.rb
+++ app/helpers/creations_helper.rb
@@ -1,2 +1,9 @@
 module CreationsHelper
+  def short_name(creation, length)
+    if( creation.name.length > length)
+      creation.name.split(//).first(length).join('') + '...'
+    else
+      creation.name
+    end
+  end
 end
