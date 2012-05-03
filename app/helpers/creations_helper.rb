da107db936aae5ff5df8455572b222eb0c6054af
mo k <mo@mokhan.ca>
2012-05-02 21:18:41 -0600

    display the first 20 characters to prevent the overflow.

--- app/helpers/creations_helper.rb
+++ app/helpers/creations_helper.rb
@@ -1,7 +1,8 @@
 module CreationsHelper
   def short_name(creation, length)
     if( creation.name.length > length)
-      creation.name.split(//).first(length).join('') + '...'
+      #creation.name.split(//).first(length).join('') + '...'
+      creation.name[0...length] + '...'
     else
       creation.name
     end
