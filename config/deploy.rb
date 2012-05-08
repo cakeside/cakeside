4d2e189e27e415b2527fbbcfba3265ab5bee7542
mo k <mo@mokhan.ca>
2012-05-08 07:54:05 -0600

    got the deployment to staging working.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 set :rvm_ruby_string, 'ruby-1.9.2-p290@cakeside'
 set :rvm_type, :system
-require 'bundler/capistrano'                 # loads RVM's capistrano plugin
+#require 'bundler/capistrano'                 # loads RVM's capistrano plugin
 set :stages, %w(production staging)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
@@ -23,7 +23,7 @@ set :deploy_env, 'production'
 set :scm_verbose, true
 
 # ugly workaround for bug https://github.com/capistrano/capistrano/issues/81
-before "deploy:assets:precompile", "bundle:install"
+before "deploy:assets:precompile", "update_bundler"
 
 # remove old releases
-after "deploy", "deploy:cleanup"
+#after "deploy", "deploy:cleanup"
