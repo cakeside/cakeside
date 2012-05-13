4eab10bb9eb7430798add4b5d7bb4971607dfd66
mo k <mo@mokhan.ca>
2012-05-12 23:17:25 -0600

    fix path production server.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
 require "rvm/capistrano"
 set :rvm_ruby_string, 'ruby-1.9.2-p290@cakeside'
 set :rvm_type, :system
