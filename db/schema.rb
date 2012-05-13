cfd9e1ee3a7e2e200ed8c6dd14c7395219cee253
mista mo <cake-eater@mokhan.ca>
2012-05-12 22:21:27 -0600

    update schema.rb file.

--- db/schema.rb
+++ db/schema.rb
@@ -56,8 +56,8 @@ ActiveRecord::Schema.define(:version => 20120301034745) do
   create_table "creations", :force => true do |t|
     t.string   "name"
     t.text     "story"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.integer  "user_id"
     t.string   "image"
   end
@@ -92,8 +92,8 @@ ActiveRecord::Schema.define(:version => 20120301034745) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
     t.string   "name"
   end
 
