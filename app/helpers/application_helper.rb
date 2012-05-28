4a27553c8fbe85f290ea1ad79464ecc21e3e46af
mo k <mo@mokhan.ca>
2012-05-27 21:40:08 -0600

    rip apart application layout into separate partials.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?&d=#{CGI.escape(default_url)}"
   end
+  def full_title(title)
+    if( title.blank? )
+      "Welcome to CakeSide, a place for cake enthusiasts to share their proud creations!"
+    else
+      "#{title} - CakeSide"
+    end
+  end
 end
