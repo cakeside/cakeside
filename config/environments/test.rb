16541101ef987828f528f9706ef3cfcc91a0b903
mo k <mo@mokhan.ca>
2012-06-06 20:56:30 -0600

    update environments config files with latest for rails 3.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ Cake::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   config.action_mailer.default_url_options = { :host => 'www.blah.com'}
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 end
