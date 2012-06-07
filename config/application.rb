da3dda8c790d810b66bcd1b10536b45023b9aa7f
mo k <mo@mokhan.ca>
2012-06-06 21:33:03 -0600

    change assets path in dev to /dev-assets from default /assets

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,8 @@ module Cake
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.to_prepare {
       Devise::SessionsController.layout "application"
       # Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "users" }
