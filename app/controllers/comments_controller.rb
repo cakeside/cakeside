c093f68ba91f87fef80f7189c9efc1039929388f
mo k <mo@mokhan.ca>
2012-06-08 19:04:20 -0600

    fix bug where all comments were submitted as user with id 1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,8 +7,9 @@ class CommentsController < ApplicationController
 
   def create
     creation = Creation.find(params[:creation_id])
-    comment = Comment.build_from(creation, current_user, params[:comment][:body])
+    comment = Comment.build_from(creation, current_user.id, params[:comment][:body])
     if comment.save
+      flash[:notice] = "Your comment was added!"
       redirect_to creation_path(creation)
     else
       flash[:error] = "Ooops... we couldn't save your comment at this time."
