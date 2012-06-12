f6ace8f959305dcfd7f022b783e3257b0a8d70ce
mo k <mo@mokhan.ca>
2012-06-12 07:13:25 -0600

    fix titles

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,21 +1,12 @@
 require 'spec_helper'
-#module Devise
-#class RegistrationsController
-
-#end
-#end
 
 describe RegistrationsController do
-  before (:each) do
-    #request.env['warden'] = mock(Warden, :authenticate => user, :authenticate! => user)
-  end
-  #let(:user){ FactoryGirl.build(:user) }
-  let(:user){ User.new( :id => 2) }
+  let(:sut) { RegistrationsController.new }
+  let(:user){ fake }
 
   context "when updating a users profile settings not including their password" do
     it "should update their website" do
-      controller = RegistrationsController.new
-      controller.stub(:current_user).and_return(user)
+      sut.stub(:current_user).and_return(user)
 
       payload = {
         :current_password => '',
@@ -28,9 +19,9 @@ describe RegistrationsController do
         :website => 'http://mokhan.ca/',
       }
 
-      #user.should_receive[:update_without_password].with(payload)
-      #put :update, :id => user.id, :user => payload
+      user.should have_received(:update_without_password,payload)
     end
+
     it "should not change their password" do
 
     end
