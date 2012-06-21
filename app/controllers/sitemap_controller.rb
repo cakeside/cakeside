17759c8c28a6d655a023680075b80d7e47fabb91
mo k <mo@mokhan.ca>
2012-06-21 06:27:26 -0600

    add creations gallery to home page if not signed in.

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -6,7 +6,6 @@ class SitemapController < ApplicationController
     @base_url = "http://#{request.host_with_port}"
     headers['Content-Type'] = 'application/xml'
     respond_to do |format|
-      format.html
       format.xml
     end
   end
