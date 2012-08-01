4b10bfdede677d359d41178b44ac6373b04d96db
mo k <mo@mokhan.ca>
2012-07-31 21:18:39 -0600

    remove favorites controller specs from photos controller file.

--- spec/routing/photos_routing_spec.rb
+++ spec/routing/photos_routing_spec.rb
@@ -11,15 +11,3 @@ describe PhotosController do
     end
   end
 end
-describe FavoritesController do
-  describe "routing" do
-    it "should route to create" do
-      get('creations/1/favorites').should 
-        route_to(:controller => "favorites", :action => "index", :creation_id => 1, :method => "GET")
-    end
-    it "should route to delete" do
-      post('creations/1/favorites').should 
-        route_to(:controller => "favorites", :action => "create", :creation_id => 1, :method => "POST")
-    end
-  end
-end
