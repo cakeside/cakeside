a949bdde86eda243962c26047aa92c4607c53b3f
mo k <mo@mokhan.ca>
2012-07-31 20:24:15 -0600

    add spec for root route

--- spec/routing/creations_routing_spec.rb
+++ spec/routing/creations_routing_spec.rb
@@ -2,6 +2,9 @@ require "spec_helper"
 
 describe CreationsController do
   describe "routing" do
+    it "is the root of the website" do
+      get('/').should route_to("creations#index")
+    end
 
     it "recognizes and generates #index" do
       { :get => "/creations" }.should route_to(:controller => "creations", :action => "index")
