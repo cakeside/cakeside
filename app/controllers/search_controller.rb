ee38c957bef3208efd0e986ec3b35ff82c4a8126
mo k <mo@mokhan.ca>
2012-08-07 06:50:12 -0600

    add artists tab and remove lat/long from profiles#index.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,6 @@ class SearchController < ApplicationController
       else
         sql = "%"+params[:q]+"%"
         @creations = Creation.where("upper(name) like upper(?) OR upper(story) like upper(?)", sql, sql).page(params[:page]).per(8)
-        ## @creations = Creation.search(params[:q], :include => :user, :match_mode => :extended, :star => true).page(params[:page]).per(6)
         @members = User.where("upper(name) like upper(?)", sql).page(params[:page]).per(8)
         @search = params[:q]
         format.html
