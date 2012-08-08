dd648e4e00da6216bb8d9d8e1d61abea663d4737
mo k <mo@mokhan.ca>
2012-08-07 21:36:45 -0600

    add avatars uploader to avatar.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120808032550) do
+ActiveRecord::Schema.define(:version => 20120808033601) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20120808032550) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "avatar"
   end
 
   create_table "categories", :force => true do |t|
