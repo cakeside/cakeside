3f731dc127e1bde2a1eb4c1fb40eadca07e7539e
mo k <mo@mokhan.ca>
2012-08-07 21:04:26 -0600

    apply settings for new avatar uploader.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -11,14 +11,25 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.production?
+    storage :fog
+  elsif Rails.env.staging?
+    storage :fog
+  elsif Rails.env.development?
+    storage :file
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  process :set_content_type
+  process :resize_to_fit => [1160, 870]
+  process :convert => 'png'
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -42,14 +53,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
   #   "something.jpg" if original_filename
   # end
-
+  def filename
+    if original_filename 
+      super.chomp(File.extname(super)) + '.png'
+    end
+  end
 end
