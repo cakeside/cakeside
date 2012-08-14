228b5bc3e2706534ff789e3f8baf421ea3524215
mo k <mo@mokhan.ca>
2012-08-14 16:19:33 -0600

    fix profiles routing spec.

--- spec/routing/profiles_routing_spec.rb
+++ spec/routing/profiles_routing_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 describe ProfilesController do
   describe "routing" do
     it "routes to index" do
-      get('/profiles/').should route_to("profiles#index", :method => "GET")
+      get('/profiles/').should route_to("profiles#index")
     end
     it "routes to #show" do
-      get("/profiles/1").should route_to("profiles#show", :id => "1", :method => 'GET')
+      get("/profiles/1").should route_to("profiles#show", :id => "1")
     end
     it "routes to my profile" do
       get("mine").should route_to("profiles#mine", :method => 'GET')
