bc7688ca5a2933156a9b49cf815f6c72b2f7e7d4
mo k <mo@mokhan.ca>
2012-08-17 09:32:01 -0600

    allow for non authenticated users to view the artists page.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_filter :authenticate_user!, :except => [:show]
+  before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
     @profiles = User.all.reverse
