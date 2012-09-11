2d1896ab2d2e84a12235b86ff6a06b68ebeb8355
mo k <mo@mokhan.ca>
2012-09-10 21:26:29 -0600

    fix broken view specs.

--- spec/views/tutorials/index.html.erb_spec.rb
+++ spec/views/tutorials/index.html.erb_spec.rb
@@ -19,8 +19,7 @@ describe "tutorials/index" do
   it "renders a list of tutorials" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Heading".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Url".to_s, :count => 2
+    assert_select "h3", :text => "Heading".to_s, :count => 2
+    assert_select "p", :text => "MyText".to_s, :count => 2
   end
 end
