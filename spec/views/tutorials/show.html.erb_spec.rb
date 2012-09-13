c6ae7e3aaa4b8562c27e2fddf126001228fe24e4
mo k <mo@mokhan.ca>
2012-09-12 21:42:07 -0600

    fix broken spec.

--- spec/views/tutorials/show.html.erb_spec.rb
+++ spec/views/tutorials/show.html.erb_spec.rb
@@ -5,13 +5,13 @@ describe "tutorials/show" do
     @tutorial = assign(:tutorial, stub_model(Tutorial,
       :heading => "Heading",
       :description => "MyText",
-      :url => "Url"
+      :url => "Url",
+      :image_url => "http://placehold.it/300x200"
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Heading/)
     rendered.should match(/MyText/)
     rendered.should match(/Url/)
