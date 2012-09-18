657e20a5b0fc342b16eec28e4d796e66b86b1927
mo k <mo@mokhan.ca>
2012-09-18 13:12:08 -0600

    convert greeting class to coffeescript.

--- spec/javascripts/utility/greeting_spec.js
+++ spec/javascripts/utility/greeting_spec.js
@@ -1,16 +1,16 @@
 describe ("Greeting", function() {
   beforeEach (function() {
-    sut = new Greeting();
+    sut = new Greeting('mo');
   });
   var sut;
   describe ("when greeting someone", function() {
     it ("should say their name", function() {
-      expect(sut.greet('mo')).toEqual('hi mo');
+      expect(sut.greet()).toEqual('hi mo');
     });
   });
   describe ("when saying goodbye", function() {
     it ("should say their name", function() {
-      expect(sut.goodbye('joe')).toEqual('goodbye joe');
+      expect(sut.goodbye()).toEqual('goodbye mo');
     });
   });
 });
