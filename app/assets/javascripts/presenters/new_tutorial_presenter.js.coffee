327b72d0e9122dc1d0d989812a1c9e7a34982e7f
mo k <mo@mokhan.ca>
2012-09-18 15:40:38 -0600

    add new tag to the hidden tag list.

--- app/assets/javascripts/presenters/new_tutorial_presenter.js.coffee
+++ app/assets/javascripts/presenters/new_tutorial_presenter.js.coffee
@@ -3,7 +3,10 @@ class window.NewTutorialPresenter
 
   present:() ->
     @view.add_tag_button.bind 'click', (event) =>
-      @view.tag_list.append(this.create_list_item_for(@view.tag_input.val()))
+      new_tag = @view.tag_input.val()
+      @view.tag_list.append(this.create_list_item_for(new_tag))
+      current = @view.hidden_tag_list.val()
+      @view.hidden_tag_list.val( new_tag )
 
   create_list_item_for:(tag) ->
     '<li><a href="#" class="label">'+tag+'</a></li>'
--- spec/javascripts/presenters/new_tutorial_presenter_spec.js
+++ spec/javascripts/presenters/new_tutorial_presenter_spec.js
@@ -4,6 +4,7 @@ describe ("NewTutorialPresenter", function() {
       add_tag_button: $('<button id="add-tag-button">'),
       tag_list: $('<ul id="tag-list">'),
       tag_input: $('<input type="text" />'),
+      hidden_tag_list: $('<input type="hidden" />'),
     };
     sut = new NewTutorialPresenter(view);
   });
@@ -12,6 +13,9 @@ describe ("NewTutorialPresenter", function() {
   describe ("when the add tag button is clicked", function() {
     it ("should add the new tag to the list of tags", function() {
       expect(view.tag_list.html()).toEqual('<li><a href="#" class="label">ruby</a></li>');
+    });
+    it ("should add the new tag to the hidden list of tags", function() {
+      expect(view.hidden_tag_list.val()).toEqual('ruby');
     });
     beforeEach (function() {
       sut.present();
