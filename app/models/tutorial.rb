85e55fdbce0a4dddbaf9fa042f04b27392ec2d71
mo k <mo@mokhan.ca>
2012-09-20 07:10:14 -0600

    add friendly url link for tutorials.

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,8 @@ class Tutorial < ActiveRecord::Base
   attr_accessible :description, :heading, :url, :image_url
   belongs_to :user
   acts_as_taggable
+
+  def to_param
+    "#{id}-#{description.gsub(/[^a-z0-9]+/i, '-')}"
+  end
 end
