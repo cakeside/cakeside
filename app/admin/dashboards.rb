b1f3813afbf032b97300c46c4a1105b48f840ebf
mo k <mo@mokhan.ca>
2012-09-25 07:34:48 -0600

    clean up the active admin dashboard.

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,2 +1,17 @@
-ActiveAdmin::Dashboards.build do
+ActiveAdmin.register_page "Dashboard" do
+  menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Creations" do
+          para "recently added creations:"
+           ul do
+             Creation.all.first(16).map do |creation|
+               li link_to(creation.name, admin_creation_path(creation))
+             end
+           end
+        end
+      end
+    end
+  end
 end
