3acc4a9cee6df10f00ed86ae1b68e2304bc607e9
mo k <mo@mokhan.ca>
2012-10-07 08:32:09 -0600

    use delayed_job to process the additional photo upload.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to :creation
   validates :image,  :presence => true
   mount_uploader :image, PhotoUploader
+
+  after_save :enqueue
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,6 +1,8 @@
 # encoding: utf-8
+require File.join(Rails.root, "lib", "carrier_wave", "delayed_job")
 
 class PhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::Delayed::Job
   include CarrierWave::RMagick
   include CarrierWave::MimeTypes
 
