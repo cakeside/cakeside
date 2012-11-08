757cddf51849342d53fa7895675a8e080dc05403
mo k <mo@mokhan.ca>
2012-11-08 07:52:47 -0700

    move the migration job to the commands folder.

--- db/migrate/20121010190225_migrate_images_for_each_creation.rb
+++ db/migrate/20121010190225_migrate_images_for_each_creation.rb
@@ -1,14 +1,3 @@
-class MigratePrimaryImage < Struct.new(:creation_id)
-  def perform
-    creation = Creation.find(creation_id)
-    photo = creation.photos.build({})
-    photo.created_at = creation.created_at
-    photo.updated_at = creation.updated_at
-    photo.image = creation.image.file
-    photo.save!
-  end
-end
-
 class MigrateImagesForEachCreation < ActiveRecord::Migration
   def up
     Creation.all.each_with_index do |creation, index|
