c4cf2d0d6a3c387c20c7c955e6430d5dfcb26331
mo k <mo@mokhan.ca>
2012-11-07 20:34:25 -0700

    remove call to creation#publish.

--- spec/requests/logins_spec.rb
+++ spec/requests/logins_spec.rb
@@ -7,24 +7,21 @@ describe "Logins" do
       response.body.should include("Got an account? Login!")
     end
   end
-  
+
   describe "Signup for a new user account" do
-  	it "should make a new user present a confirmation" do
-  		visit "/login"
-  		
-  		within(".form-horizontal") do
-	  		fill_in('user_name', :with => 'John Smith')
-	  		fill_in('user_city', :with => 'Calgary, Alberta, Canada')
-	  		fill_in('user_email',:with => 'test@example.com')
-			fill_in('user_password', :with => 'password')
-			fill_in('user_password_confirmation', :with => 'password')
-			click_button "submit-registration"
-		end
+    it "should make a new user present a confirmation" do
+      visit "/login"
 
-		page.should have_content "You have signed up successfully"
-  	end
+      within(".form-horizontal") do
+        fill_in('user_name', :with => 'John Smith')
+        fill_in('user_city', :with => 'Calgary, Alberta, Canada')
+        fill_in('user_email',:with => 'test@example.com')
+        fill_in('user_password', :with => 'password')
+        fill_in('user_password_confirmation', :with => 'password')
+        click_button "submit-registration"
+      end
 
-  end 
-
-
+      page.should have_content "You have signed up successfully"
+    end
+  end
 end
