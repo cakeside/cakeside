052d78e5952f1f6e173c7d180bcbbe084a35a13e
mo k <mo@mokhan.ca>
2013-01-27 21:59:16 -0700

    make tutorials fit under the tags listing

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,6 @@ class TagsController < ApplicationController
   # GET /tags/fondant
   def show
     @tag = params[:id]
-    @tutorials = Tutorial.tagged_with([@tag]).page(params[:page]).per(12)
+    @tutorials = Tutorial.tagged_with([@tag])
   end
 end
