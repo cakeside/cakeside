2045defb57984c90ef6042f26c9fa1b65ce36cef
mo k <mo@mokhan.ca>
2013-02-05 21:54:58 -0700

    attempt to improve creations load time

--- app/services/queries/find_all_creations_query.rb
+++ app/services/queries/find_all_creations_query.rb
@@ -11,9 +11,10 @@ class FindAllCreationsQuery
   private
 
   def find_creations
-    Creation.all.select do |creation|
-      creation.published? && creation.is_safe_for_children?
-    end
+    Creation.joins(:photos).where(:is_restricted => false)
+    #Creation.all.select do |creation|
+      #creation.published? && creation.is_safe_for_children?
+    #end
   end
   def make_pageable(items, page)
     Kaminari.paginate_array(items).page(page).per(9)
