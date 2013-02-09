5aaeebbda6eda1aa1baa404bcd765d3c9ecce75f
mo k <mo@mokhan.ca>
2013-02-09 10:30:24 -0700

    display the tutorials and creations on the tags page.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,5 +3,6 @@ class TagsController < ApplicationController
   def show
     @tag = params[:id]
     @tutorials = Tutorial.tagged_with([@tag])
+    @creations = Creation.tagged_with([@tag])
   end
 end
