f817bcc605a4e20862c0bb887d50f58faa4e5245
mo k <mo@mokhan.ca>
2013-02-14 07:03:19 -0700

    fix paging bug

--- app/services/queries/find_all_creations_query.rb
+++ app/services/queries/find_all_creations_query.rb
@@ -4,7 +4,8 @@ class FindAllCreationsQuery
     @mapper = mapper
   end
   def fetch(params)
-    make_pageable(find_creations, params[:page])
+    find_creations.page(params[:page]).per(9)
+    #make_pageable(find_creations, params[:page])
     #make_pageable(find_creations.map {|c| @mapper.map_from(c) }, params[:page])
   end
 
@@ -12,9 +13,6 @@ class FindAllCreationsQuery
 
   def find_creations
     Creation.joins(:photos).where(:is_restricted => false).uniq
-    #Creation.all.select do |creation|
-      #creation.published? && creation.is_safe_for_children?
-    #end
   end
   def make_pageable(items, page)
     Kaminari.paginate_array(items).page(page).per(9)
