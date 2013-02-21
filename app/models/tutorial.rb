d754e1867f7a9852dcfd4fa5c0f4bc4eb07cea81
mo k <mo@mokhan.ca>
2013-02-20 20:51:04 -0700

    make sure proper tutorial_tags element is passed to the controller.

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,5 +1,6 @@
 class Tutorial < ActiveRecord::Base
   attr_accessible :description, :heading, :url, :image_url, :user_id, :author, :author_url
+  validates :url,  :presence => true
   belongs_to :user
   acts_as_taggable
 
