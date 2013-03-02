f72fb346be72a8c6bca160f70a28d75826f5ef08
mo khan <mo@mokhan.ca>
2013-03-02 10:43:11 -0700

    remove commented out code

--- app/services/queries/find_all_creations_query.rb
+++ app/services/queries/find_all_creations_query.rb
@@ -2,18 +2,14 @@ class FindAllCreationsQuery
   def initialize(mapper = CreationToDisplayCreationDTOMapper.new)
     @mapper = mapper
   end
+
   def fetch(params)
     find_creations.page(params[:page]).per(9)
-    #make_pageable(find_creations, params[:page])
-    #make_pageable(find_creations.map {|c| @mapper.map_from(c) }, params[:page])
   end
 
   private
 
   def find_creations
     Creation.joins(:photos).where(:is_restricted => false).uniq
-  end
-  def make_pageable(items, page)
-    Kaminari.paginate_array(items).page(page).per(9)
   end
 end
