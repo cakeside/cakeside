9384e078c856d0c5d480b6f5c879c7959c671a7c
mo khan <mo@mokhan.ca>
2013-03-18 22:31:17 -0600

    redirect to tags if category is not found

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,10 @@ class CategoriesController < ApplicationController
   # GET /categories/fondant
   def show
     @category = Category.where(:slug => params[:id]).first
-    @creations = Creation.joins(:categories).where(:categories => {:slug => params[:id]}).page(params[:page]).per(12)
+    if @category
+      @creations = Creation.joins(:categories).where(:categories => {:slug => params[:id]}).page(params[:page]).per(12)
+    else
+      redirect_to "/tags/#{params[:id]}"
+    end
   end
 end
