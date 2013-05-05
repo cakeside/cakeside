5f9d4254fc7b9fe7e38be8ca877044cd8a02565c
mo khan <mo@mokhan.ca>
2013-05-04 22:37:16 -0600

    eager fetch avatar in artists listing

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   before_filter :authenticate_user!, :except => [:index, :show]
 
   def index
-    @profiles = User.page(params[:page]).per(12)
+    @profiles = User.includes(:avatar).page(params[:page]).per(12)
   end
 
   def show
