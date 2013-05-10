8e7824702910e09d48fa95a577b94cb148fddceb
mo khan <mo@mokhan.ca>
2013-05-10 16:53:22 -0600

    add home controller spec

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe HomeController do
+  it "should render the about us page" do
+    get "about_us"
+    response.should be_success
+  end
+
+  it "should render the why cakeside page" do
+    get "why_cakeside"
+    response.should be_success
+  end
+end
