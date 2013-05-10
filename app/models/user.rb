027652ba990372a32b310befdf77755fcbf48dcd
mo khan <mo@mokhan.ca>
2013-05-10 17:39:34 -0600

    unmount authentications from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ActiveRecord::Base
   has_many :creations, :dependent => :destroy
   has_many :favorites, :dependent => :destroy
   has_many :tutorials, :dependent => :destroy
-  has_many :authentications
   has_and_belongs_to_many :interests, :join_table => 'users_interests', :uniq => true, :autosave => true
   has_one :avatar
   acts_as_tagger
