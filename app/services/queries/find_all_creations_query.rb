f09545e786658eb8019ec226b8264d92645a261a
mo khan <mo@mokhan.ca>
2013-06-02 07:56:55 -0600

    optimize queries on homepage

--- app/services/queries/find_all_creations_query.rb
+++ app/services/queries/find_all_creations_query.rb
@@ -10,6 +10,6 @@ class FindAllCreationsQuery
   private
 
   def find_creations
-    Creation.includes(:user).joins(:photos).where(:is_restricted => false).uniq.includes(:user)
+    Creation.includes(:user, :photos).where(:is_restricted => false).uniq
   end
 end
