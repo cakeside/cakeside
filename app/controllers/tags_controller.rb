58f08fce08188b59f9e2a930b3fa128f9938feb2
mo khan <mo@mokhan.ca>
2013-06-17 21:28:12 -0600

    replace ' ' with '-' when looking up a tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
-    @tag = params[:id]
+    @tag = params[:id].gsub(/ /, '-')
     @total_tutorials = Tutorial.tagged_with(@tag).count
     @total_creations = Creation.tagged_with(@tag).count
     @creations = Creation.includes(:user).tagged_with([@tag]).page(params[:page]).per(15)
