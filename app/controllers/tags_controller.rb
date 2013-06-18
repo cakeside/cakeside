3d99d63bbc8101b7e5171e1f4416f5ad5e28dd95
mo khan <mo@mokhan.ca>
2013-06-17 21:29:05 -0600

    downcase the tag before looking it up

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def show
-    @tag = params[:id].gsub(/ /, '-')
+    @tag = params[:id].downcase.gsub(/ /, '-')
     @total_tutorials = Tutorial.tagged_with(@tag).count
     @total_creations = Creation.tagged_with(@tag).count
     @creations = Creation.includes(:user).tagged_with([@tag]).page(params[:page]).per(15)
