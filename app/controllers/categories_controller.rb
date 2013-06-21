6242207c912b6bd2278b8a8d7ad7c87a197c3faf
mo khan <mo@mokhan.ca>
2013-06-21 12:34:17 -0600

    load all cakes right off the category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,6 @@
 class CategoriesController < ApplicationController
-  # GET /categories/fondant
   def show
     @category = Category.find_by_slug(params[:id])
-    if @category
-      @creations = Creation.includes(:user).joins(:categories).where(:categories => {:slug => params[:id]}).page(params[:page]).per(12)
-    else
-      redirect_to "/tags/#{params[:id]}"
-    end
+    @creations = @category.creations.includes(:user).page(params[:page]).per(12)
   end
 end
