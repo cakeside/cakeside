d4dc24b033c2d30ac51fd9015155f77b98cd5333
mo khan <mo@mokhan.ca>
2013-06-21 15:19:18 -0600

    order categories by name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,12 @@ module ApplicationHelper
       image_tag(user.avatar.avatar.thumb.url, alt: user.name, class: options[:class], :style => "width:#{options[:size]}px;")
     end
   end
+
   def gravatar_for(user, options = { size: 260 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     image_tag("https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}&d=mm", alt: user.name, class: 'gravatar')
   end
+
   def full_title(title)
     if( title.blank? )
       "CakeSide - for cake lovers!"
