269e2484e13bda682d90848e4dc2e2513ed5708f
mo khan <mo@mokhan.ca>
2013-06-21 23:17:11 -0600

    fix routing spec

--- spec/routing/categories_routing_spec.rb
+++ spec/routing/categories_routing_spec.rb
@@ -3,7 +3,8 @@ require "spec_helper"
 describe CategoriesController do
   describe "routing" do
     it "routes to #show" do
-      get("/categories/fondant").should route_to({"id"=>"fondant", "controller"=>"categories", "action"=>"show"})
+      get("/categories/fondant").should route_to({:slug => "fondant", :controller => "categories", :action => "show"})
+      get("/categories/fondant/page/1").should route_to({:slug => "fondant", :controller => "categories", :action => "show", :page => "1"})
     end
   end
 end
