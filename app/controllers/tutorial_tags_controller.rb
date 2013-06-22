2eca070761cf097e9cfb8ccf3dd179473b138d21
mo khan <mo@mokhan.ca>
2013-06-22 08:37:05 -0600

    create tutorial tags controller

---
+++ app/controllers/tutorial_tags_controller.rb
@@ -0,0 +1,8 @@
+class TutorialTagsController < ApplicationController
+  def show
+    @tag = params[:id].downcase.gsub(/ /, '-')
+    @total_tutorials = Tutorial.tagged_with(@tag).count
+    @total_creations = Creation.tagged_with(@tag).count
+    @tutorials = Tutorial.includes(:tags).tagged_with(@tag).page(params[:page]).per(15)
+  end
+end
