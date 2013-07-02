479872b620a1f347bff17433d379b6632c747de3
mo khan <mo@mokhan.ca>
2013-07-01 19:42:36 -0700

    check if current_user is available

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,6 +1,6 @@
 class Avatar < ActiveRecord::Base
   include PublicActivity::Model
-  tracked owner: Proc.new{ |controller, model| controller.current_user }
+  tracked owner: Proc.new{ |controller, model| controller.current_user if controller }
   belongs_to :user
   mount_uploader :avatar, AvatarUploader
 
