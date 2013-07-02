fff4ce481b200fe757a934f1537c8eb9381e86e0
mo khan <mo@mokhan.ca>
2013-07-02 12:44:44 -0700

    display map with points for other nearby users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,10 @@ class ProfilesController < ApplicationController
     @user = User.find(params[:id])
     @creations = @user.creations.includes([:user, :photos]).page(params[:page]).per(18)
     @nearby_users = @user.nearbys(50) || []
+    @map_url = "http://maps.google.com/maps/api/staticmap?size=450x300&sensor=false&markers=#{@user.latitude}%2C#{@user.longitude}"
+    @nearby_users.each do |user|
+      @map_url += "&markers=#{user.latitude}%2C#{user.longitude}"
+    end
   end
 
   def favorites
