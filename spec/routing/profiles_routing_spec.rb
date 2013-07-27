a9e445a372e71be3f1aaeb7f1dd0048a66df90e3
mo khan <mo@mokhan.ca>
2013-07-26 19:54:43 -0600

    fix profile routing spec

--- spec/routing/profiles_routing_spec.rb
+++ spec/routing/profiles_routing_spec.rb
@@ -4,11 +4,13 @@ describe ProfilesController do
     it "routes to index" do
       get('/profiles/').should route_to("profiles#index")
     end
+
     it "routes to #show" do
       get("/profiles/1").should route_to("profiles#show", :id => "1")
     end
+
     it "routes to my favorites" do
-      get("favorites").should route_to("profiles#favorites", :method => "GET")
+      get("/favorites").should route_to("profiles#favorites")
     end
   end
 end
