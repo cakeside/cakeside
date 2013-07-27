63b76b2c366e833312b5fa747e4d00eb10398938
mo khan <mo@mokhan.ca>
2013-07-27 14:41:28 -0600

    fix path to unicorn log file

--- config/recipes/unicorn.rb
+++ config/recipes/unicorn.rb
@@ -1,7 +1,7 @@
 set_default(:unicorn_user) { user }
 set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_config) { "#{shared_path}/config/unicorn.rb" }
-set_default(:unicorn_log) { "#{shared_path}/log/unicorn.rb" }
+set_default(:unicorn_log) { "#{shared_path}/log/unicorn.log" }
 set_default(:unicorn_workers, 4)
 
 namespace :unicorn do
