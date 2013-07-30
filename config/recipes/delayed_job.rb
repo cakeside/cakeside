f6f45b6726ffa4991043d7979123e1cdc00bfe01
mo khan <mo@mokhan.ca>
2013-07-29 21:53:31 -0600

    add init.d script for starting, stopping and restarting delayed_job

--- config/recipes/delayed_job.rb
+++ config/recipes/delayed_job.rb
@@ -1,9 +1,17 @@
 namespace :delayed_job do 
+  desc "setup delayed job"
+  task :setup, roles: :app do
+    template "delayed_job_init.erb", "/tmp/delayed_job_init"
+    run "chmod +x /tmp/delayed_job_init"
+    run "#{sudo} mv /tmp/delayed_job_init /etc/init.d/delayed_job_#{application}"
+    run "#{sudo} update-rc.d -f delayed_job_#{application} defaults"
+  end
+  after "deploy:setup", "delayed_job:setup"
+
   %w[start stop restart].each do |command|
     desc "#{command} delayed_job"
-    task command, :roles => :app do
-      #run "chmod +x #{current_path}/script/restart_delayed_job"
-      run "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job #{command}"
+    task command, roles: :app do
+      run "#{sudo} service delayed_job_#{application} #{command}"
     end
     after "deploy:#{command}", "delayed_job:#{command}"
   end
