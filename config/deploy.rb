cfd6592fd801010e041b64504962c9436c0d4a0b
mo khan <mo@mokhan.ca>
2013-08-31 08:31:53 -0600

    add logrotate recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ load "config/recipes/delayed_job"
 #load "config/recipes/rvm"
 load "config/recipes/rbenv"
 load "config/recipes/monit"
+load "config/recipes/logrotate"
 load "config/tasks/utility"
 load "config/tasks/rails"
 
