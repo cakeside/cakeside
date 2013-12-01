da5affad0741ab4acc319669d16f5877e813a056
mo khan <mo@mokhan.ca>
2013-11-30 20:06:46 -0700

    use existing class method for query.

--- app/services/queries/find_all_creations_query.rb
+++ app/services/queries/find_all_creations_query.rb
@@ -1,19 +1,9 @@
 class FindAllCreationsQuery
-  def initialize(repository = Creation.where(nil))
+  def initialize(repository = Creation)
     @repository = repository
   end
 
   def fetch(params)
-    find_creations.page(params[:page]).per(9)
-  end
-
-  private
-
-  def find_creations
-    @repository
-    .includes(:user, :photos)
-    .where(:is_restricted => false)
-    .where('photos_count > 0')
-    .uniq
+    @repository.visible_creations.page(params[:page]).per(9)
   end
 end
