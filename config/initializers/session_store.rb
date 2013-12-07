8f8ebd83584acc0a70125b474566db0ef4e4750b
mo khan <mo@mokhan.ca>
2013-12-06 17:19:17 -0700

    use memcache session storage.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
-
-Cake::Application.config.session_store :cookie_store, key: '_cake_eater'
+configuration = {
+  key: '_cake_eater',
+  expire_after: 1.day
+}
+Cake::Application.config.session_store ActionDispatch::Session::CacheStore, configuration
