09f3207b1354722936e6650259d387e95eed9a7b
mo khan <mo@mokhan.ca>
2013-12-29 16:19:12 -0700

    comment out unused action.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,16 +4,16 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   # is this action used any more?
-  def update
-    @user = current_user
-    @user.interest_ids = params[:user][:interest_ids] ||= []
-    if @user.update_without_password(params[:user])
-      sign_in @user, :bypass => true
-      redirect_to profile_path(current_user), :notice =>'Your settings have been updated successfully!'
-    else
-      render "edit"
-    end
-  end
+  #def update
+    #@user = current_user
+    #@user.interest_ids = params[:user][:interest_ids] ||= []
+    #if @user.update_without_password(params[:user])
+      #sign_in @user, :bypass => true
+      #redirect_to profile_path(current_user), :notice =>'Your settings have been updated successfully!'
+    #else
+      #render "edit"
+    #end
+  #end
 
   def after_sign_in_path_for(resource)
     settings_path
