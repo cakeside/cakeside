3397f3b311f6ab4182619b180b690e8991dbcbcc
mo khan <mo@mokhan.ca>
2013-12-28 19:07:43 -0700

    add cache-control and etags to tutorial tags.

--- app/controllers/creation_tags_controller.rb
+++ app/controllers/creation_tags_controller.rb
@@ -11,6 +11,6 @@ class CreationTagsController < ApplicationController
     @total_creations = Creation.tagged_with(@tag).count
     @creations = Creation.includes([:user, :tags, :photos]).tagged_with([@tag]).where('photos_count > 0').page(params[:page]).per(15)
     expires_in(6.hours)
-    fresh_when(@tag)
+    fresh_when(@creations)
   end
 end
