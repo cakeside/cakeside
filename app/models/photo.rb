7cef00586af74ad2d78c7289f3cb1f51f5a368fd
mo khan <mo@mokhan.ca>
2013-12-29 18:57:15 -0700

    add image_processing column to photo model to tell if an image has been processed in the background and use store_in_background to speed up upload.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,31 +2,26 @@ class Photo < ActiveRecord::Base
   belongs_to :creation, :counter_cache => true
   validates :image,  :presence => true
   mount_uploader :image, PhotoUploader
-  process_in_background :image
-  #store_in_background :image
+  #process_in_background :image
+  store_in_background :image
 
-  include Rails.application.routes.url_helpers
   def to_jq_upload
-    if image.thumb.url && image.thumb.url != image.thumb.default_url
-      {
-        :name => read_attribute(:image),
-        :url => image.url,
-        :thumbnail_url => image.thumb.url,
-        :delete_url => id,
-        :delete_type => "DELETE"
-      }
-    else
-      {
-        :name => read_attribute(:image),
-        :url => image.url,
-        :thumbnail_url => image.url,
-        :delete_url => id,
-        :delete_type => "DELETE"
-      }
-    end
+    {
+      :name => read_attribute(:image),
+      :url => image.url,
+      :thumbnail_url => is_processed? ? image.thumb.url : image.url,
+      :delete_url => id,
+      :delete_type => "DELETE"
+    }
   end
 
   def watermark
     creation.watermark
+  end
+
+  private
+
+  def is_processed?
+    self.image_processing
   end
 end
