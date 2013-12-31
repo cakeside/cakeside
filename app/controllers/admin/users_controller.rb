476508fcc41ada7d0a07a5c2cc6543915120d38f
mo khan <mo@mokhan.ca>
2013-12-31 13:53:05 -0700

    cache the admin/users page because it is a slow page.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,8 @@ module Admin
       @users = User.includes(:avatar).order(:created_at => :desc)
       @recent_users = @users.limit(10)
       @active_users = @users.order(:updated_at).limit(10)
+      expires_in(10.minutes)
+      fresh_when(User.maximum(:updated_at)) if User.any?
     end
   end
 end
