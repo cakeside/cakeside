8407ed7c3c87a13fd36c6e3eb111a9b41168f4d1
mo khan <mo@mokhan.ca>
2013-12-31 22:31:18 -0700

    collapse methods to load header data into one method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,30 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :load_categories
-  before_filter :load_tutorials
-  before_filter :load_users
+  before_filter :load_header
   before_filter :configure_permitted_parameters, if: :devise_controller?
-
-  private
-
-  def load_categories
-    @categories = Category.all
-  end
-
-  def load_tutorials
-    @newest_tutorials = Tutorial.order(:created_at => :desc).limit(3)
-  end
-
-  def load_users
-    @newest_members = User.order(:created_at => :desc).limit(3)
-    @top_members = User.order(:creations_count => :desc).limit(3)
-  end
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:user) { |u| u.permit(:name, :city, :email) }
+  end
+
+  private
+
+  def load_header
+    @categories = Category.all
+    @newest_tutorials = Tutorial.order(:created_at => :desc).limit(3)
+    @newest_members = User.order(:created_at => :desc).limit(3)
+    @top_members = User.order(:creations_count => :desc).limit(3)
   end
 end
 
