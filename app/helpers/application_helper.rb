cb9911aaa9a5b6300eb420d201394ba1396c14a3
mo khan <mo@mokhan.ca>
2013-12-31 18:42:26 -0700

    change link to username to a link to an avatar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def avatar_for(user, options = {size: 260, class:''})
-    return gravatar_for(user, options) if user.avatar == nil || !user.avatar.avatar.present?
-    image_tag(user.avatar.avatar.thumb.url, alt: user.name, class: options[:class], :style => "width:#{options[:size]}px;")
+  def avatar_for(user:nil, size: 260, clazz: '')
+    return gravatar_for(user, size) if user.avatar == nil || !user.avatar.avatar.present?
+    image_tag(user.avatar.avatar.thumb.url, alt: user.name, class: clazz, :style => "width:#{size}px;")
   end
 
-  def gravatar_for(user, options = { size: 260 })
+  def gravatar_for(user, size)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    image_tag("https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}&d=mm", alt: user.name, class: 'gravatar')
+    image_tag("https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=mm", alt: user.name, class: 'gravatar')
   end
 
   def full_title(title)
