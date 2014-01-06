d9f79b7b92770021d367bb0e8e4074aa5726b1a4
mo khan <mo@mokhan.ca>
2014-01-05 22:01:15 -0700

    fix broken specs.

--- spec/services/queries/find_all_creations_query_spec.rb
+++ spec/services/queries/find_all_creations_query_spec.rb
@@ -4,9 +4,14 @@ describe FindAllCreationsQuery do
   let(:sut) { FindAllCreationsQuery.new }
   let(:results) { sut.fetch({}) }
 
-  let!(:cake_with_a_photo) { create(:creation, photos: [create(:photo)]) }
+  let!(:cake_with_a_photo) { create(:creation) }
   let!(:cake_without_a_photo) { create(:creation, photos: []) }
   let!(:restricted_cake) { create(:creation, is_restricted: true, photos: [create(:photo)]) }
+
+  before :each do
+    cake_with_a_photo.photos << create(:photo)
+    cake_with_a_photo.photos.first.update_attribute(:image_processing, nil)
+  end
 
   it "returns all creations with at least one photo" do
     results.should include(cake_with_a_photo)
