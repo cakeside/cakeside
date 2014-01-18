d3aa634c32b253c10bff5034e3a03b20c58f901e
mo khan <mo@mokhan.ca>
2014-01-18 09:54:38 -0700

    add default url host for emails in dev environment.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,7 @@ Cake::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.smtp_settings = {
     :address              => ENV['SMTP_HOST'],
     :port                 => ENV['SMTP_PORT'],
