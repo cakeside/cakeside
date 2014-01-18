d8e65a1927437a9e8db1ca34b3f2e512d2e1190c
mo khan <mo@mokhan.ca>
2014-01-18 08:53:42 -0700

    remove unused js startup code.

--- app/assets/javascripts/creations.js
+++ app/assets/javascripts/creations.js
@@ -1,17 +1,4 @@
 var Creations = Creations || {};
 Creations.Show = {
-  initialize: function(){
-    $('.gallery').colorbox({
-      rel:'gallery',
-      width:function(){
-        var width = '100%', viewport = $(window).width();
-        if (viewport > 960) {
-          width = '50%';
-        } else if (viewport >= 768) {
-          width = '80%';
-        }
-        return width;
-      }
-    });
-  }
+  initialize: function(){ }
 };
