4127c89dd5ad46541d8a924042eb4c6b17ae58e8
mo khan <mo@mokhan.ca>
2014-01-18 22:56:10 -0700

    update text in notification email.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,6 @@ class NotificationMailer < ActionMailer::Base
   def notification_email(activity)
     @user = activity.user
     @activity = activity
-   mail(to: @user.email, subject: "New Activity on CakeSide")
+    mail(to: @user.email, subject: "New Activity on CakeSide")
   end
 end
