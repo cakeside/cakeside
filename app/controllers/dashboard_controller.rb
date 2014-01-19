fa804838b6092635a12aa082067157cdba49d98b
mo khan <mo@mokhan.ca>
2014-01-19 09:24:51 -0700

    display my creations on the dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @creations = current_user.creations
     @activities = current_user.recent_activities
   end
 end
