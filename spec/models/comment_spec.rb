e0ce9c098d0b82e2ae999b4e80f11c82fa65f78d
mo khan <mo@mokhan.ca>
2014-01-18 22:54:37 -0700

    create a new activity when a comment is created.

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe Comment do
+  describe "#create" do
+    let(:creation) { create(:creation) }
+    let(:user) { creation.user }
+
+    it "creates a new activity" do
+      comment = Comment.create(user: user, creation: creation)
+      comment.activities.count.should == 1
+    end
+  end
+end
