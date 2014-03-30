32c3a58ff8a0b495773c491eeb996b3279622d2f
mo khan <mo@mokhan.ca>
2014-03-30 15:32:30 -0600

    change usages from 2.0.0 to 2.1.1

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -3,7 +3,7 @@ echo "load bashrc"
 source ~/.bashrc
 
 echo 'switch gemset'
-rvm use ruby-2.0.0-p353@cakeside --create
+rvm use ruby-2.1.1@cakeside --create
 
 echo 'bundle install'
 bundle exec rake ci:deploy:staging --trace
