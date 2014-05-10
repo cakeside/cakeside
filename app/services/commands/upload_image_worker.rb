ba9fd29c8047639edb84b56434a2c7839922f7c3
mo khan <mo@mokhan.ca>
2014-05-10 09:42:53 -0600

    add email to .env.template and remove extra data params when sending an error.

--- app/services/commands/upload_image_worker.rb
+++ app/services/commands/upload_image_worker.rb
@@ -1,5 +1,5 @@
 class UploadImageWorker < ::CarrierWave::Workers::StoreAsset
   def error(job, exception)
-    ExceptionNotifier.notify_exception(exception, :data => {:worker => worker.to_s, :queue => queue, :payload => payload})
+    ExceptionNotifier.notify_exception(exception)
   end
 end
