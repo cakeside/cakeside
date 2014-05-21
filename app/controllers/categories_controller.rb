cb20f2700bbb7721148d48a13b8b05130e331be3
mo khan <mo@mokhan.ca>
2014-05-20 21:48:49 -0600

    add migration to move categories from old join table to category_id on creations table.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by_slug(params[:slug].downcase)
     if @category
-      @categories = Category.all
       @creations = @category.creations.includes(:user).page(params[:page]).per(12)
     else
       redirect_to creation_tag_path(params[:slug].downcase)
