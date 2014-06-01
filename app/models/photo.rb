0b7903c7356af470b8e9d28e944c051195feb5a5
mo khan <mo@mokhan.ca>
2014-05-31 21:01:07 -0600

    remove usages of carrierwave backgrounder.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,6 @@
 class Photo < ActiveRecord::Base
-  belongs_to :creation, :counter_cache => true, touch: true
-  #validates :image,  :presence => true
+  belongs_to :creation, counter_cache: true, touch: true
   mount_uploader :image, PhotoUploader
-  #process_in_background :image if Rails.env.test?
-  #store_in_background :image, UploadImageWorker unless Rails.env.test?
 
   def thumb_url
     image.thumb.url
