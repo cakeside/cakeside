12ef8499a762930aaf3c47bc70963eb1b15354ae
mo khan <mo@mokhan.ca>
2014-06-04 22:27:40 -0600

    use mini magick to resize original to large image and upload to s3.

--- app/services/application/handlers/process_photo.rb
+++ app/services/application/handlers/process_photo.rb
@@ -26,5 +26,46 @@ class ProcessPhoto
   def upload_original(file, photo)
     key = "uploads/photo/image/#{photo.id}/#{File.basename(file)}"
     @blob_storage.upload(key, file)
+    upload_large_version(file, photo)
+  end
+
+  def upload_large_version(file, photo)
+    key = "uploads/photo/image/#{photo.id}/large_#{File.basename(file)}"
+    Image.new(file).resize_to_fit(570, 630)
+    @blob_storage.upload(key, file)
+  end
+
+end
+
+class Image
+  attr_reader :current_path
+
+  def initialize(current_path)
+    @current_path = current_path
+  end
+
+  def resize_to_fit(width, height)
+    manipulate! do |img|
+      img.resize "#{width}x#{height}"
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
+  private
+
+  def manipulate!
+    image = ::MiniMagick::Image.open(current_path)
+    begin
+      image = yield(image)
+      image.write(current_path)
+      image.run_command("identify", current_path)
+    ensure
+      image.destroy!
+    end
+  rescue ::MiniMagick::Error, ::MiniMagick::Invalid => e
+    default = I18n.translate(:"errors.messages.mini_magick_processing_error", :e => e, :locale => :en)
+    message = I18n.translate(:"errors.messages.mini_magick_processing_error", :e => e, :default => default)
+    raise CarrierWave::ProcessingError, message
   end
 end
