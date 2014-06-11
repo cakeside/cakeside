00b43f5fedc45ef837e287dcf41cacfe3e643375
mo khan <mo@mokhan.ca>
2014-06-10 20:41:42 -0600

    upgrade to rspec rails 3.0

---
+++ spec/rails_helper.rb
@@ -0,0 +1,42 @@
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+require 'ffaker'
+require 'database_cleaner'
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {:timeout => 60})
+end
+Capybara.javascript_driver = :poltergeist if ENV['HEADLESS']
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.use_transactional_fixtures = false
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+  config.include DeviseHelper::Controllers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+  config.infer_spec_type_from_file_location!
+  config.raise_errors_for_deprecations!
+end
