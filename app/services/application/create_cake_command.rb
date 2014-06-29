cff3acf182741c31891bdbb5c9ef505a2b917f5a
mo khan <mo@mokhan.ca>
2014-06-28 21:38:11 -0600

    extract method to create cake and ensure the category exists first.

--- app/services/application/create_cake_command.rb
+++ app/services/application/create_cake_command.rb
@@ -5,8 +5,9 @@ class CreateCakeCommand
     @message_bus = message_bus
   end
 
-  def run(creation_attributes, tags)
-    cake = @current_user.creations.create(creation_attributes)
+  def run(attributes, tags)
+    cake = @current_user.create_cake(name: attributes[:name], description: attributes[:story], category: Category.find(attributes[:category_id]), watermark: attributes[:watermark])
+    cake.hide_from_children! if attributes[:is_restricted].present? && attributes[:is_restricted] == true
     @current_user.tag(cake, with: tags, on: :tags)
 
     if cake.save
