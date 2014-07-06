dbeff20aa6ef128d46483df835edd33540ce3dd6
mo khan <mo@mokhan.ca>
2014-07-05 21:42:42 -0600

    remove dead code and tidy up controller action.

--- app/services/application/upload_photo.rb
+++ app/services/application/upload_photo.rb
@@ -8,6 +8,7 @@ class UploadPhoto
     photo = @cakes.find(cake_id).photos.create!(image_processing: true, watermark: params[:watermark])
     @message_bus.publish(:upload_photo, create_message_from(cake_id, params, photo))
     map_from(photo.id, params[:image].original_filename)
+    photo
   end
 
   private
