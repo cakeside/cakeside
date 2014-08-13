d6afcc295747490f3ceddb718bb823d986c31a23
mo khan <mo@mokhan.ca>
2014-08-12 20:36:53 -0600

    enable uuid extension.

--- db/migrate/20140807003036_create_sessions.rb
+++ db/migrate/20140807003036_create_sessions.rb
@@ -1,5 +1,6 @@
 class CreateSessions < ActiveRecord::Migration
   def change
+    enable_extension 'uuid-ossp'
     create_table :sessions, id: :uuid do |t|
       t.integer :user_id
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20140807003036) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
   create_table "activities", force: true do |t|
     t.integer  "subject_id",   null: false
     t.string   "subject_type", null: false
@@ -21,20 +25,20 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.datetime "updated_at",   null: false
   end
 
-  add_index "activities", ["subject_id"], name: "index_activities_on_subject_id"
-  add_index "activities", ["subject_type"], name: "index_activities_on_subject_type"
-  add_index "activities", ["user_id"], name: "index_activities_on_user_id"
+  add_index "activities", ["subject_id"], name: "index_activities_on_subject_id", using: :btree
+  add_index "activities", ["subject_type"], name: "index_activities_on_subject_type", using: :btree
+  add_index "activities", ["user_id"], name: "index_activities_on_user_id", using: :btree
 
   create_table "avatars", force: true do |t|
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "avatar"
     t.boolean  "avatar_processing"
     t.string   "avatar_tmp"
   end
 
-  add_index "avatars", ["user_id"], name: "index_avatars_on_user_id"
+  add_index "avatars", ["user_id"], name: "index_avatars_on_user_id", using: :btree
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -52,8 +56,8 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["creation_id"], name: "index_comments_on_creation_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["creation_id"], name: "index_comments_on_creation_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "creations", force: true do |t|
     t.string   "name"
@@ -68,8 +72,8 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.integer  "category_id"
   end
 
-  add_index "creations", ["created_at"], name: "index_creations_on_created_at"
-  add_index "creations", ["user_id"], name: "index_creations_on_user_id"
+  add_index "creations", ["created_at"], name: "index_creations_on_created_at", using: :btree
+  add_index "creations", ["user_id"], name: "index_creations_on_user_id", using: :btree
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0
@@ -81,11 +85,11 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "favorites", force: true do |t|
     t.integer  "user_id"
@@ -94,13 +98,13 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.datetime "updated_at"
   end
 
-  add_index "favorites", ["creation_id"], name: "index_favorites_on_creation_id"
-  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id"
+  add_index "favorites", ["creation_id"], name: "index_favorites_on_creation_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "interests", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "photos", force: true do |t|
@@ -118,10 +122,13 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.string   "watermark"
   end
 
-  add_index "photos", ["creation_id"], name: "index_photos_on_creation_id"
+  add_index "photos", ["creation_id"], name: "index_photos_on_creation_id", using: :btree
 
-# Could not dump table "sessions" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "sessions", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
@@ -133,11 +140,11 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-  add_index "taggings", ["taggable_type"], name: "index_taggings_on_taggable_type"
-  add_index "taggings", ["tagger_id"], name: "index_taggings_on_tagger_id"
-  add_index "taggings", ["tagger_type"], name: "index_taggings_on_tagger_type"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+  add_index "taggings", ["taggable_type"], name: "index_taggings_on_taggable_type", using: :btree
+  add_index "taggings", ["tagger_id"], name: "index_taggings_on_tagger_id", using: :btree
+  add_index "taggings", ["tagger_type"], name: "index_taggings_on_tagger_type", using: :btree
 
   create_table "tags", force: true do |t|
     t.string "name"
@@ -148,22 +155,22 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.text     "description"
     t.string   "url"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image_url"
     t.string   "author"
     t.string   "author_url"
   end
 
-  add_index "tutorials", ["user_id"], name: "index_tutorials_on_user_id"
+  add_index "tutorials", ["user_id"], name: "index_tutorials_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",     limit: 128, default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0
+    t.integer  "sign_in_count",                      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -182,28 +189,28 @@ ActiveRecord::Schema.define(version: 20140807003036) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0
+    t.integer  "failed_attempts",                    default: 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
     t.string   "invitation_token"
     t.string   "full_address"
-    t.integer  "creations_count",        default: 0
+    t.integer  "creations_count",                    default: 0
     t.boolean  "is_admin"
   end
 
-  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["created_at"], name: "index_users_on_created_at"
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["created_at"], name: "index_users_on_created_at", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
   create_table "users_interests", id: false, force: true do |t|
     t.integer "user_id"
     t.integer "interest_id"
   end
 
-  add_index "users_interests", ["interest_id", "user_id"], name: "index_users_interests_on_interest_id_and_user_id"
+  add_index "users_interests", ["interest_id", "user_id"], name: "index_users_interests_on_interest_id_and_user_id", using: :btree
 
 end
