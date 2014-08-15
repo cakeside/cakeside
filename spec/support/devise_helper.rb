68ffcbcfa173c49c1a7f811e1b56a947988cee6f
mo khan <mo@mokhan.ca>
2014-08-14 22:06:55 -0600

    adjust auth helper to stub out application controller methods.

--- spec/support/devise_helper.rb
+++ spec/support/devise_helper.rb
@@ -2,8 +2,9 @@ module DeviseHelper
   module Controllers
     def http_login(user)
       new_session = UserSession.new
-      controller.stub(:authenticate!).and_return(new_session)
+      controller.stub(:user_session).and_return(new_session)
       controller.stub(:current_user).and_return(user)
+      controller.stub(:user_signed_in?).and_return(true)
     end
   end
 end
