a945abca1afddb209814475439e50229ffb74feb
mo khan <mo@mokhan.ca>
2014-08-14 21:58:46 -0600

    update bullet and configure it to show warnings in footer instead of popup.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,10 +40,10 @@ Cake::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
+    Bullet.alert = false
     Bullet.bullet_logger = true
     Bullet.console = true
-    #Bullet.growl = true
     Bullet.rails_logger = true
+    Bullet.add_footer = true
   end
 end
