1947b93eb09a9dca5133e1f460fc7450ba328429
mo khan <mo@mokhan.ca>
2014-08-18 20:56:46 -0600

    update jenkins db config.

--- script/ci.sh
+++ script/ci.sh
@@ -2,5 +2,5 @@
 
 echo 'bundle install'
 bundle install --without production
-cp config/database.yml.example config/database.yml
+cp config/database.yml.jenkins config/database.yml
 bundle exec rake ci --trace
