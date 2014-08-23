862f2cb16a498bd284d0ab8feea320adef7b0c2e
mo khan <mo@mokhan.ca>
2014-08-22 21:18:50 -0600

    move old cap to old_cap folder.

--- Gemfile
+++ Gemfile
@@ -38,8 +38,12 @@ gem 'ejs', '~> 1.1.1'
 gem 'js-routes', '~> 0.9.8'
 
 group :development do
-  gem 'capistrano', '~> 2.15.5'
-  gem 'capistrano-ext', '~> 1.2.1'
+  #gem 'capistrano', '~> 2.15.5'
+  #gem 'capistrano-ext', '~> 1.2.1'
+  gem 'capistrano', '~> 3.0', require: false
+  gem 'capistrano-rails', '~> 1.1', require: false
+  gem 'capistrano-bundler', '~> 1.1', require: false
+  gem 'capistrano-rbenv', '~> 2.0', require: false
   gem 'bullet'
   gem 'better_errors'
   gem 'lol_dba'
---
+++ old_cap/deploy.rb
@@ -0,0 +1,38 @@
+require 'bundler/capistrano'
+require 'dotenv/deployment/capistrano'
+
+load "config/recipes/environments"
+load "config/recipes/base"
+load "config/recipes/nginx"
+load "config/recipes/unicorn"
+load "config/recipes/postgresql"
+load "config/recipes/delayed_job"
+load "config/recipes/monit"
+load "config/tasks/utility"
+load "config/tasks/rails"
+
+set :application, "cakeside"
+set :user, "deployer"
+set :use_sudo, false
+default_run_options[:pty] = true # password prompt
+
+# git
+set :scm, :git
+set :scm_verbose, true
+set :repository,  "git@bitbucket.org:cakeside/cakeside.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+
+#copy
+#set :scm, :none
+#set :repository, "."
+#set :deploy_via, :copy
+
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :keep_releases, 3
+set :normalize_asset_timestamps, false
+set :ssh_options, {:forward_agent => true}
+
+after "deploy", "deploy:cleanup" # remove old releases
+
+require './config/boot'
