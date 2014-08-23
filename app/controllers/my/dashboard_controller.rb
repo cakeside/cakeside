3b7ca34bd32aeb853b5cf1ac38eefe58bd12d274
mo khan <mo@mokhan.ca>
2014-08-23 13:26:22 -0600

    fix select n+1 on dashboard.

--- app/controllers/my/dashboard_controller.rb
+++ app/controllers/my/dashboard_controller.rb
@@ -1,7 +1,7 @@
 module My
   class DashboardController < BaseController
     def index
-      @items = (current_user.creations + current_user.tutorials).sort_by! { |x| x.created_at }.reverse
+      @items = (current_user.creations.includes(:photos) + current_user.tutorials).sort_by! { |x| x.created_at }.reverse
       @activities = current_user.recent_activities
     end
   end
