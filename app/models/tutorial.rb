d3e7c18045bfc4572637079f56f58e2430279f42
mo khan <mo@mokhan.ca>
2014-09-02 11:10:25 -0600

    order the tutorials from newest to oldest.

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,6 +2,7 @@ class Tutorial < ActiveRecord::Base
   validates :url, presence: true
   belongs_to :user
   acts_as_taggable
+  default_scope -> { order('tutorials.created_at desc') }
 
   def to_param
     "#{id}-#{heading.gsub(/[^a-z0-9]+/i, '-')}"
