0e8f2a2134b547e22c46ee8c2c13d14e9c8c8f09
mo khan <mo@mokhan.ca>
2014-09-10 20:50:53 -0600

    fix select n+1 in activities page.

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   class ActivitiesController < AdminController
     def index
-      @recent_activities = Activity.order(:created_at => :desc).limit(100)
+      @recent_activities = Activity.includes(subject: [{user: :avatar}, :creation]).order(created_at: :desc).limit(100)
       expires_in(10.minutes)
     end
   end
