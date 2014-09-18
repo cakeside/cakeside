0f2c9843f1e11e70bdf29709f302b5911d58594b
mo khan <mo@mokhan.ca>
2014-09-17 21:49:14 -0600

    login the user after registering.

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 describe RegistrationsController do
   describe "#create" do
+    let(:user_session) { double(access: SecureRandom.hex(12)) }
+
     it 'creates a new user' do
+      User.stub(:login).with('mo@cakeside.com', 'password').and_return(user_session)
+
       post :create, user: { name: 'mo', email: 'mo@cakeside.com', password: 'password', password_confirmation: 'password' }
 
       expect(User.count).to eql(1)
       expect(response).to redirect_to(my_root_path)
+      expect(cookies.signed[:raphael]).to_not be_nil
+      expect(cookies.signed[:raphael]).to eql(user_session.access)
     end
 
     it 'displays errors' do
