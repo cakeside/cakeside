c3e9c2606a77d7313c326f9abdcaeb3620d16726
mo khan <mo@mokhan.ca>
2014-09-18 11:13:46 -0600

    create the password reset mailer.

--- app/mailers/password_reset_mailer.rb
+++ app/mailers/password_reset_mailer.rb
@@ -1,4 +1,8 @@
-class PasswordResetMailer
+class PasswordResetMailer < ActionMailer::Base
+  default from: "noreply@cakeside.com"
+
   def send_password_reset_instructions_to(user)
+    @user = user
+    mail(to: @user.email, subject: "CakeSide - Password Reset Request")
   end
 end
---
+++ spec/mailers/password_reset_mailer_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe PasswordResetMailer do
+  describe '#send_password_reset_instructions_to' do
+    let(:user) { build(:user) }
+    let(:mail) { PasswordResetMailer.send_password_reset_instructions_to(user) }
+
+    it "adds a subject" do
+      expect(mail.subject).to eql("CakeSide - Password Reset Request")
+    end
+
+    it "sends to the users email" do
+      expect(mail.to).to include(user.email)
+    end
+
+    it "should send from the correct address" do
+      expect(mail.from).to include('noreply@cakeside.com')
+    end
+
+    it "includes their name" do
+      expect(mail.body.encoded).to match(user.name)
+    end
+  end
+end
