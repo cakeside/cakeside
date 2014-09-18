80c11ee12d2325ae765b26d7ed7409a9ea842253
mo khan <mo@mokhan.ca>
2014-09-17 22:03:37 -0600

    fix the registartion spec.

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -8,13 +8,12 @@ describe "Registration", :js => true do
         fill_in('user_name', :with => 'John Smith')
         fill_in('user_email',:with => Faker::Internet.email)
         fill_in('user_password', :with => 'password')
-        fill_in('user_password_confirmation', :with => 'password')
         click_button "submit-registration"
       end
     end
 
     it "should let you register with that email address" do
-      page.should have_content "You have signed up successfully"
+      expect(page).to have_content("No new activity to report.")
     end
 
     xit "should take you to the settings page" do
