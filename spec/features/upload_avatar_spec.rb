d0e492ebebb97fd012c445c18cc18d6a6f5fbe83
mo khan <mo@mokhan.ca>
2014-09-21 21:10:49 -0600

    fix broken spec.

--- spec/features/upload_avatar_spec.rb
+++ spec/features/upload_avatar_spec.rb
@@ -12,7 +12,7 @@ describe "uploading an avatar", js: true do
     click_button("Sign In")
     visit new_my_avatar_path
     file = File.expand_path(File.join(Rails.root, '/spec/fixtures/images/gorilla.jpg'))
-    within(".edit_photo") do
+    within(".new_photo") do
       attach_file('photo_image', file)
     end
     click_button "Upload picture"
