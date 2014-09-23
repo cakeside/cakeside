5dfbb2fc806023eceb6cfca0597cff3d55c9e6f7
mo khan <mo@mokhan.ca>
2014-09-22 19:50:24 -0600

    add a bit of console output and add missing create tmp dir method.

--- script/migrate-photos.rb
+++ script/migrate-photos.rb
@@ -11,12 +11,18 @@ class Command
 
   def run
     Photo.find_each do |photo|
-      original = OriginalVersion.new(photo)
-      storage.download(original.create_key) do |file|
-        message_bus.publish(:upload_photo, {
-          photo_id: photo.id,
-          file_path: move_to_temporary_storage(file.path, File.basename(original.create_key))
-        })
+      begin
+        original = OriginalVersion.new(photo)
+        key = original.create_key
+        puts "processing #{key}"
+        storage.download(key) do |file|
+          bus.publish(:upload_photo, {
+            photo_id: photo.id,
+            file_path: move_to_temporary_storage(file.path, File.basename(key))
+          })
+        end
+      rescue StandardError => error
+        puts error.message
       end
     end
   end
@@ -26,6 +32,12 @@ class Command
   def move_to_temporary_storage(temp_file_path, original_filename)
     "#{create_tmp_dir}/#{original_filename}".tap do |new_path|
       FileUtils.mv(temp_file_path, new_path)
+    end
+  end
+
+  def create_tmp_dir
+    Rails.root.join("tmp/uploads/#{SecureRandom.uuid}").tap do |directory|
+      system "mkdir -p #{directory}"
     end
   end
 end
