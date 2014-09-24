d621467b4552098cd928900f6f69a258feb9867e
mo khan <mo@mokhan.ca>
2014-09-23 20:58:21 -0600

    move email validator to a separate file.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,5 @@
 require 'bcrypt'
 
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class User < ActiveRecord::Base
   include BCrypt
   before_save :ensure_authentication_token
