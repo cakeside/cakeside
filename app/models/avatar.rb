0e9e437ea83a9f7f3288aa1d6c2a0ed19535def3
mo khan <mo@mokhan.ca>
2014-09-28 20:20:04 -0600

    remove usage of old avatar uploader.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,6 +1,6 @@
 class Avatar < ActiveRecord::Base
   belongs_to :user
-  mount_uploader :avatar, AvatarUploader
+  #mount_uploader :avatar, AvatarUploader
 
   def attach(file)
     self.avatar=file
