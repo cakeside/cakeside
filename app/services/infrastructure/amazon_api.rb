06a03905f9171e6d61cb2937acb9d48cc166e439
mo khan <mo@mokhan.ca>
2014-10-13 12:48:45 -0600

    create amazon product abstraction to parse the xml response.

--- app/services/infrastructure/amazon_api.rb
+++ app/services/infrastructure/amazon_api.rb
@@ -7,6 +7,38 @@ class AmazonAPI
 
   def search(query)
     return [] if query.blank?
-    client.search(Keywords: query, SearchIndex: :Kitchen, Sort: :salesrank, Availability: :Available, MerchantId: :Amazon)
+    results = client.search(Keywords: query, SearchIndex: :Kitchen, Sort: :salesrank, Availability: :Available, MerchantId: :Amazon)
+    results.map { |x| AmazonProduct.new(x) }
+  end
+end
+
+class AmazonProduct
+  attr_reader :item
+  delegate :asin, :detail_page_url, :item_attributes, :item_links, to: :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def title
+    item.item_attributes.title
+  end
+
+  def url
+    item.detail_page_url
+  end
+
+  def manufacturer
+    item.item_attributes.manufacturer
+  end
+
+  def category
+    item.item_attributes.product_group
+  end
+
+  def links
+    item.item_links.item_link.map do |link|
+      OpenStruct.new(description: link.description, url: link.url)
+    end
   end
 end
