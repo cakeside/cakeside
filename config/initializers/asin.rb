a8f0c11dd54ca68c3059bc908e61da3b9a83fe74
mo khan <mo@mokhan.ca>
2014-10-13 11:02:38 -0600

    add amazon product api gems, configuration and initializer.

---
+++ config/initializers/asin.rb
@@ -0,0 +1,5 @@
+ASIN::Configuration.configure do |config|
+  config.key           = ENV.fetch('AWS_PRODUCT_KEY')
+  config.secret        = ENV.fetch('AWS_PRODUCT_SECRET')
+  config.associate_tag = ENV.fetch('AWS_PRODUCT_ASSOCIATE_ID')
+end
