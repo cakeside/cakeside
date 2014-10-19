719298c8681793496f12c13777069d02aef8e4b4
mo khan <mo@mokhan.ca>
2014-10-18 22:11:48 -0600

    use base class paginatable.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
   end
 
   def index
-    @profiles = repository.search_with(params).page(params[:page]).per(12)
+    @profiles = repository.search_with(params).page(page).per(per_page)
   end
 
   def show
