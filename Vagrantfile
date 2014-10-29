0b38e66a9dd3208bf4fe9eab23b2cc8bcae4341f
mo khan <mo@mokhan.ca>
2014-10-28 21:17:53 -0600

    update vagrant stuff.

--- Vagrantfile
+++ Vagrantfile
@@ -2,8 +2,7 @@
 # vi: set ft=ruby :
 
 Vagrant.configure("2") do |config|
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64_vmware.box"
+  config.vm.box = "phusion/ubuntu-14.04-amd64"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update -y
+apt-get upgrade -y
 apt-get -y install curl git-core python-software-properties imagemagick libmagickwand-dev memcached
 apt-get -y install build-essential
 apt-get -y install tklib
@@ -17,7 +18,7 @@ apt-get -y update
 apt-get -y install nginx
 add-apt-repository -y ppa:pitti/postgresql
 apt-get -y update
-apt-get -y install postgresql-9.2 libpq-dev
+apt-get -y install postgresql-9.3 libpq-dev
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get -y update
 apt-get -y install nodejs
@@ -30,3 +31,10 @@ apt-get -y install zlib1g-dev libssl-dev
 apt-get -y install libreadline-gplv2-dev
 # Install some nokogiri dependencies:
 apt-get -y install libxml2 libxml2-dev libxslt1-dev
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install 2.1.3
