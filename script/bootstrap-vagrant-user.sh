984d9852da21908beedf47bcee5cd1237a55a01d
mo khan <mo@mokhan.ca>
2014-10-31 12:02:29 -0600

    install missing packages and create vagrant db.

--- script/bootstrap-vagrant-user.sh
+++ script/bootstrap-vagrant-user.sh
@@ -6,7 +6,18 @@ git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-bu
 git clone https://github.com/tpope/rbenv-ctags.git ~/.rbenv/plugins/rbenv-ctags
 git clone https://github.com/rkh/rbenv-update.git ~/plugins/rbenv-update
 git clone https://github.com/sstephenson/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+rbenv update
 rbenv install 2.1.4
-gem install bundler
+rbenv global 2.1.4
+gem install bundler --no-ri --no-rdoc
+rbenv rehash
 cd /vagrant
-bundle install
+bundle install -j 4
+rbenv rehash
+if [ ! -f /vagrant/config/database.yml ]; then
+  ln -s config/database.yml.vagrant config/database.yml
+fi
+if [ ! -f /vagrant/.env ]; then
+  ln -s .env.example .env
+fi
+bundle exec rake db:create db:migrate db:seed
--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -8,19 +8,27 @@ apt-get -y install zlib1g-dev libssl-dev
 apt-get -y install libreadline-gplv2-dev
 apt-get -y install libxml2 libxml2-dev libxslt1-dev
 apt-get -y install gawk libreadline6-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config libffi-dev
-apt-get -y autoremove
-add-apt-repository -y ppa:nginx/stable
-apt-get -y update
-apt-get -y install nginx
-add-apt-repository -y ppa:pitti/postgresql
-apt-get -y update
-apt-get -y install postgresql-9.3 libpq-dev
-add-apt-repository -y ppa:chris-lea/node.js
-apt-get -y update
-apt-get -y install nodejs
 apt-get -y install build-essential
 apt-get -y install tklib
 apt-get -y install zlib1g-dev libssl-dev
 apt-get -y install libreadline-gplv2-dev
 apt-get -y install libxml2 libxml2-dev libxslt1-dev
+apt-get -y install curl libcurl3 libcurl3-gnutls libcurl4-openssl-dev
+apt-get -y autoremove
+
+add-apt-repository -y ppa:nginx/stable
+apt-get -y update
+apt-get -y install nginx
+
+add-apt-repository -y ppa:pitti/postgresql
+apt-get -y update
+apt-get -y install postgresql-9.3 libpq-dev postgresql-contrib-9.3
+
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get -y update
+apt-get -y install nodejs
+
+su postgres<<EOF
+createuser -s -e vagrant
+EOF
 su -c "source /vagrant/script/bootstrap-vagrant-user.sh" vagrant
