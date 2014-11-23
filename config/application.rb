e0cf5c6c3e2afbd4eb01a976d5906edc8a2c58bb
mo khan <mo@mokhan.ca>
2014-11-22 22:26:44 -0700

    fix the CORS configuration.

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,8 @@ module Cake
     config.middleware.use Rack::Cors do
       allow do
         origins "*"
-        resource "api/*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+        resource "/api/v2/*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+        resource "/api/v2/*/*", headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
     config.middleware.use ExceptionNotification::Rack, :email => {
