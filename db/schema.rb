19e5272cb4295605ff2df1516f3c9ebd35c518bd
luu <luuduong@gmail.com>
2014-12-06 15:35:55 +0000

    add constraints and indexes to tools table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202033956) do
+ActiveRecord::Schema.define(version: 20141206152512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 20141202033956) do
 
   create_table "avatars", force: true do |t|
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "avatar"
     t.boolean  "avatar_processing"
     t.string   "avatar_tmp"
@@ -85,8 +85,8 @@ ActiveRecord::Schema.define(version: 20141202033956) do
     t.datetime "failed_at"
     t.string   "locked_by"
     t.string   "queue"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
@@ -103,8 +103,8 @@ ActiveRecord::Schema.define(version: 20141202033956) do
 
   create_table "interests", force: true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "locations", id: :uuid, default: "uuid_generate_v4()", force: true do |t|
@@ -160,20 +160,23 @@ ActiveRecord::Schema.define(version: 20141202033956) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "tools", force: true do |t|
-    t.string   "name"
+    t.string   "name",        null: false
     t.text     "description"
-    t.string   "asin"
+    t.string   "asin",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "tools", ["asin"], name: "index_tools_on_asin", using: :btree
+  add_index "tools", ["name"], name: "index_tools_on_name", unique: true, using: :btree
 
   create_table "tutorials", force: true do |t|
     t.string   "heading"
     t.text     "description"
     t.string   "url"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image_url"
     t.string   "author"
     t.string   "author_url"
@@ -198,8 +201,8 @@ ActiveRecord::Schema.define(version: 20141202033956) do
   add_index "user_sessions", ["user_id"], name: "index_user_sessions_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "password_digest",        default: "", null: false
+    t.string   "email",                              default: "", null: false
+    t.string   "password_digest",        limit: 128, default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "created_at"
@@ -211,7 +214,7 @@ ActiveRecord::Schema.define(version: 20141202033956) do
     t.string   "city"
     t.string   "authentication_token"
     t.string   "full_address"
-    t.integer  "creations_count",        default: 0
+    t.integer  "creations_count",                    default: 0
     t.boolean  "admin"
   end
 
