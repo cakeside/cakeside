bfdc92d9a73f966b15857f2e21cfa89955c4ba28
mo khan <mo@mokhan.ca>
2015-01-01 20:57:55 -0700

    upgrade to ruby 2.2.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ set :keep_releases, 3
 set :ssh_options, { forward_agent: true }
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.1.5'
+set :rbenv_ruby, '2.2.0'
 
 namespace :deploy do
   desc 'Restart application'
