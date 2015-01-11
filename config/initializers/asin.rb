b2a698b9d3b11c883bea3e6d5aa14ec4c8f6e389
mo khan <mo@mokhan.ca>
2015-01-11 00:08:57 -0700

    exclude loading amazon product api initializer in tests.

--- config/initializers/asin.rb
+++ config/initializers/asin.rb
@@ -1,7 +1,9 @@
-ASIN::Configuration.configure do |config|
-  config.key           = ENV.fetch('AWS_PRODUCT_KEY')
-  config.secret        = ENV.fetch('AWS_PRODUCT_SECRET')
-  config.associate_tag = ENV.fetch('AWS_PRODUCT_ASSOCIATE_ID')
+unless Rails.env.test?
+  ASIN::Configuration.configure do |config|
+    config.key           = ENV.fetch('AWS_PRODUCT_KEY')
+    config.secret        = ENV.fetch('AWS_PRODUCT_SECRET')
+    config.associate_tag = ENV.fetch('AWS_PRODUCT_ASSOCIATE_ID')
+  end
+  HTTPI.adapter = :curb
+  HTTPI.logger  = Rails.logger
 end
-HTTPI.adapter = :curb
-HTTPI.logger  = Rails.logger
