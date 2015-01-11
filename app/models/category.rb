227bdd2d5eb7532c5a5af7136f488fb410e1eabe
mo khan <mo@mokhan.ca>
2015-01-11 08:44:03 -0700

    parameterize slug.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,9 @@ class Category < ActiveRecord::Base
   has_many :creations
   default_scope -> { order(:slug) }
 
-  scope :by_slug, ->(category) { find_by(slug: category.downcase)}
+  scope :by_slug, ->(category) do
+    find_by(slug: category.downcase.parameterize)
+  end
 
   def to_param
     slug
