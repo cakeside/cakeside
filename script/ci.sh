3ca923dfa8ce012960ec3b4f257c6011b6d18742
mo khan <mo@mokhan.ca>
2015-01-21 20:39:05 -0700

    random cleanup.

--- script/ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-echo 'bundle install'
-bundle install --without production
-cp config/database.yml.jenkins config/database.yml
-RAILS_ENV=test bundle exec rake ci --trace
--- script/deploy-production.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -x
-bundle exec cap production deploy
