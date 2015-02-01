5959ca39a647f42f834cb253acb85c1c69409364
mo khan <mokhan@users.noreply.github.com>
2015-02-01 12:56:36 -0700

    extract local variables

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,8 @@ class RegistrationsController < ApplicationController
   def create
     user = User.create(secure_params)
     if user.save
-      session[:raphael] = User.login(secure_params[:email], secure_params[:password]).access(request)
+      email, password = secure_params[:email], secure_params[:password]
+      session[:raphael] = User.login(email, password).access(request)
       redirect_to my_dashboard_path
     else
       flash[:error] = user.errors.full_messages
