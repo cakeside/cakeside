698f0d659a352df76ba8e76e619df1778c4def6e
mo khan <mo@mokhan.ca>
2015-02-02 20:29:38 -0700

    remove long commented out line.

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module Cake
     # config.time_zone = "Central Time (US & Canada)"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join("my", "locales", "*.{rb,yml}").to_s]
     translations_path = Rails.root.join("config", "locales", "**", "*.{rb,yml}")
     config.i18n.load_path += Dir[translations_path]
     # config.i18n.default_locale = :de
