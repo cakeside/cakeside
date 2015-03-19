cf395d37f89144f3daf0cfec43ae7d27b30a60bd
mo khan <mo@mokhan.ca>
2015-03-18 21:24:44 -0600

    hound happy.

--- app/controllers/tutorial_tags_controller.rb
+++ app/controllers/tutorial_tags_controller.rb
@@ -5,6 +5,9 @@ class TutorialTagsController < ApplicationController
 
   def show
     @tag = params[:id].downcase.gsub(/ /, '-')
-    @tutorials = Tutorial.tagged_with(@tag).includes([:tags]).page(params[:page]).per(12)
+    @tutorials = Tutorial.
+      tagged_with(@tag).
+      includes([:tags]).
+      page(page).per(per_page)
   end
 end
