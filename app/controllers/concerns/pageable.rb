dbfddd2b1b245eadfed7900dce057104f61fa140
mo khan <mo@mokhan.ca>
2016-02-27 09:21:24 -0700

    ensure page and per_page return integers.

--- app/controllers/concerns/pageable.rb
+++ app/controllers/concerns/pageable.rb
@@ -3,11 +3,11 @@ module Pageable
   DEFAULT_PER_PAGE=12
 
   def page
-    params[:page]
+    (params[:page] || 1).to_i
   end
 
   def per_page
-    params[:per_page] || DEFAULT_PER_PAGE
+    (params[:per_page] || DEFAULT_PER_PAGE).to_i
   end
 
   def paginate(items)
