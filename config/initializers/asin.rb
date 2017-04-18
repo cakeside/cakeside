6824387188d8a8755635092b01a7229d41cc33b6
mo <mokha@cisco.com>
2017-04-17 21:45:33 -0600

    dont force aws env vars.

--- config/initializers/asin.rb
+++ config/initializers/asin.rb
@@ -1,8 +1,8 @@
 unless Rails.env.test?
   ASIN::Configuration.configure do |config|
-    config.key           = ENV.fetch('AWS_PRODUCT_KEY')
-    config.secret        = ENV.fetch('AWS_PRODUCT_SECRET')
-    config.associate_tag = ENV.fetch('AWS_PRODUCT_ASSOCIATE_ID')
+    config.key           = ENV['AWS_PRODUCT_KEY']
+    config.secret        = ENV['AWS_PRODUCT_SECRET']
+    config.associate_tag = ENV['AWS_PRODUCT_ASSOCIATE_ID']
   end
   HTTPI.adapter = :curb
   HTTPI.logger  = Rails.logger
