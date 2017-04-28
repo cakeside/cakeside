8fdd31578f85a72440985b9f8918f3999268c937
mo <mokha@cisco.com>
2017-04-27 21:15:34 -0600

    activate ruby 2.4.1 in cap deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ set :keep_releases, 3
 set :ssh_options, { forward_agent: true }
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
-set :rbenv_ruby, '2.2.0'
+set :rbenv_ruby, '2.4.1'
 
 namespace :deploy do
   desc 'Restart application'
